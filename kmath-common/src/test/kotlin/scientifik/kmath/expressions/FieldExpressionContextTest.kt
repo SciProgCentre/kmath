6df416f064828156b7323e907e1490786ed3c362
Alexander Nozik <altavir@gmail.com>
2018-08-25 21:22:07 +0300

    Added ComplexField test for expressions.

--- kmath-common/src/test/kotlin/scientifik/kmath/expressions/FieldExpressionContextTest.kt
+++ kmath-common/src/test/kotlin/scientifik/kmath/expressions/FieldExpressionContextTest.kt
@@ -1,5 +1,7 @@
 package scientifik.kmath.expressions
 
+import scientifik.kmath.operations.Complex
+import scientifik.kmath.operations.ComplexField
 import scientifik.kmath.operations.DoubleField
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -14,5 +16,16 @@ class FieldExpressionContextTest {
         }
         assertEquals(expression("x" to 1.0), 4.0)
         assertEquals(expression(), 9.0)
+    }
+
+    @Test
+    fun testComplex() {
+        val context = FieldExpressionContext(ComplexField)
+        val expression = with(context) {
+            val x = variable("x", Complex(2.0, 0.0))
+            x * x + 2 * x + 1.0
+        }
+        assertEquals(expression("x" to Complex(1.0, 0.0)), Complex(4.0, 0.0))
+        assertEquals(expression(), Complex(9.0, 0.0))
     }
 }
