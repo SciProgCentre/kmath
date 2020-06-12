047af8c172278cdf077e3e9db5f86aff3387f515
Alexander Nozik <altavir@gmail.com>
2020-06-12 11:11:13 +0300

    Fix ND extendend fields

--- kmath-ast/build.gradle.kts
+++ kmath-ast/build.gradle.kts
@@ -10,7 +10,7 @@ kotlin.sourceSets {
     commonMain {
         dependencies {
             api(project(":kmath-core"))
-            implementation("com.github.h0tk3y.betterParse:better-parse-multiplatform:0.4.0-alpha3")
+            implementation("com.github.h0tk3y.betterParse:better-parse-multiplatform:0.4.0-alpha-3")
         }
     }
 }
