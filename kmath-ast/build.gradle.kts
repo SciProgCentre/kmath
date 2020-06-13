0950580b85ca02a72c13863efc72aae2367e3ab3
Alexander Nozik <altavir@gmail.com>
2020-06-13 18:26:18 +0300

    Moe better-parse to common. Watch for https://github.com/h0tk3y/better-parse/issues/27

--- kmath-ast/build.gradle.kts
+++ kmath-ast/build.gradle.kts
@@ -11,11 +11,17 @@ kotlin.sourceSets {
         dependencies {
             api(project(":kmath-core"))
             implementation("com.github.h0tk3y.betterParse:better-parse-multiplatform:0.4.0-alpha-3")
+            implementation("com.github.h0tk3y.betterParse:better-parse-multiplatform-metadata:0.4.0-alpha-3")
         }
     }
     jvmMain{
         dependencies{
             implementation("com.github.h0tk3y.betterParse:better-parse-jvm:0.4.0-alpha-3")
+        }
+    }
+    jsMain{
+        dependencies{
+            implementation("com.github.h0tk3y.betterParse:better-parse-js:0.4.0-alpha-3")
         }
     }
 }
