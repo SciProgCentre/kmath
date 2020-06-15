2580ab347e0fc53c8c030e63c3eb8de96e52b839
Iaroslav <postovalovya@gmail.com>
2020-06-15 17:37:11 +0700

    Make ClassWriter extensions internal

--- kmath-ast/src/jvmMain/kotlin/scientifik/kmath/asm/internal/classWriters.kt
+++ kmath-ast/src/jvmMain/kotlin/scientifik/kmath/asm/internal/classWriters.kt
@@ -3,9 +3,9 @@ package scientifik.kmath.asm.internal
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.MethodVisitor
 
-inline fun ClassWriter(flags: Int, block: ClassWriter.() -> Unit): ClassWriter = ClassWriter(flags).apply(block)
+internal inline fun ClassWriter(flags: Int, block: ClassWriter.() -> Unit): ClassWriter = ClassWriter(flags).apply(block)
 
-inline fun ClassWriter.visitMethod(
+internal inline fun ClassWriter.visitMethod(
     access: Int,
     name: String,
     descriptor: String,
