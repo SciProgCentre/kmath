57245587608f10455adbd58275aadb366a37f7fd
Iaroslav Postovalov <postovalovya@gmail.com>
2020-10-04 14:10:53 +0700

    Reconfigure native build

--- kmath-gsl/build.gradle.kts
+++ kmath-gsl/build.gradle.kts
@@ -1,14 +1,18 @@
 @file:Suppress("UNUSED_VARIABLE")
 
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
-    id("ru.mipt.npm.native")
+    id("ru.mipt.npm.mpp")
 }
 
 kotlin {
-    targets.withType<KotlinNativeTarget> {
-        compilations["main"].cinterops {
+    val nativeTarget = when (System.getProperty("os.name")) {
+        "Mac OS X" -> macosX64("native")
+        "Linux" -> linuxX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    val main by nativeTarget.compilations.getting {
+        cinterops {
             val libgsl by creating {
                 defFile("src/nativeInterop/cinterop/libgsl.def")
                 includeDirs { allHeaders("./src/nativeMain/resources/") }
