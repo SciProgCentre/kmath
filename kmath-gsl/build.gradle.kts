881f2280fc697c53a6fe1c68d9e559a9a6cde538
Iaroslav Postovalov <postovalovya@gmail.com>
2020-10-04 14:39:24 +0700

    Try adding Windows support

--- kmath-gsl/build.gradle.kts
+++ kmath-gsl/build.gradle.kts
@@ -5,9 +5,12 @@ plugins {
 }
 
 kotlin {
-    val nativeTarget = when (System.getProperty("os.name")) {
-        "Mac OS X" -> macosX64("native")
-        "Linux" -> linuxX64("native")
+    val hostOs = System.getProperty("os.name")
+
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        hostOs.startsWith("Windows") -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
