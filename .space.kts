75b1eb7d3195d32b8c2f738932803cb4a258285f
Iaroslav Postovalov <postovalovya@gmail.com>
2020-10-13 22:14:40 +0700

    Update .space.kts

--- .space.kts
+++ .space.kts
@@ -1,1 +1,6 @@
-job("Build") { gradlew("openjdk:11", "build") }
+job("Build") {
+    gradlew("openjdk:11", "build") {
+        env["SPACE_USER"] = Secrets("space_user")
+        env["SPACE_TOKEN"] = Secrets("space_token")
+    }
+}
