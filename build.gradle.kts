9da3b139db8813b9529106a3c5262fe1fd88ff22
Iaroslav Postovalov <postovalovya@gmail.com>
2020-10-13 22:00:01 +0700

    Add space properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,20 @@
+import ru.mipt.npm.gradle.KSciencePublishPlugin
+
 plugins {
     id("ru.mipt.npm.project")
+    id("ru.mipt.npm.publish") apply false
 }
 
-val kmathVersion: String by extra("0.2.0-dev-2")
-val bintrayRepo: String by extra("kscience")
-val githubProject: String by extra("kmath")
+private val kmathVersion: String by extra("0.2.0-dev-2")
+private val bintrayRepo: String by extra("kscience")
+private val githubProject: String by extra("kmath")
 
 allprojects {
     repositories {
         jcenter()
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
-        maven("https://dl.bintray.com/kotlin/kotlinx")
-        maven("https://dl.bintray.com/hotkeytlt/maven")
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+        maven(url = "https://dl.bintray.com/kotlin/kotlinx")
+        maven(url = "https://dl.bintray.com/hotkeytlt/maven")
     }
 
     group = "kscience.kmath"
@@ -19,7 +22,13 @@ allprojects {
 }
 
 subprojects {
-    if (name.startsWith("kmath")) apply<ru.mipt.npm.gradle.KSciencePublishPlugin>()
+    if (name.startsWith("kmath")) apply<KSciencePublishPlugin>()
+
+    ksciencePublish {
+        spaceRepo = "https://maven.pkg.jetbrains.space/mipt-npm/p/sci/maven"
+        spaceUser = System.getenv("SPACE_USER")
+        spaceToken = System.getenv("SPACE_TOKEN")
+    }
 }
 
 readme {
