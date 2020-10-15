d4aa4587a9a5f1b417db4d7b479151f8d5663ac0
Iaroslav Postovalov <postovalovya@gmail.com>
2020-10-15 23:53:19 +0700

    Add missing files

---
+++ kmath-prob/src/commonMain/kotlin/kscience/kmath/prob/internal/InternalErf.kt
@@ -0,0 +1,11 @@
+package kscience.kmath.prob.internal
+
+import kotlin.math.abs
+
+internal object InternalErf {
+    fun erfc(x: Double): Double {
+        if (abs(x) > 40) return if (x > 0) 0.0 else 2.0
+        val ret = InternalGamma.regularizedGammaQ(0.5, x * x, 10000)
+        return if (x < 0) 2 - ret else ret
+    }
+}
