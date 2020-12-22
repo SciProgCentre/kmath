6fe344068b361e1051ab32fe052e32cb29ca054b
Iaroslav Postovalov <postovalovya@gmail.com>
2020-12-22 21:05:24 +0700

    Add branch filter

--- .space.kts
+++ .space.kts
@@ -1,4 +1,12 @@
 job("Publish") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"dev"
+            }
+        }
+    }
+
     gradlew("openjdk:11", "publish") {
         env["SPACE_USER"] = Secrets("space_user")
         env["SPACE_TOKEN"] = Secrets("space_token")
