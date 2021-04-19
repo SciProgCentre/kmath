86818f6864ac722ebd5b32f2274c9eb6afb06ca0
Alexander Nozik <altavir@gmail.com>
2021-04-19 09:31:56 +0300

    Replace dl4j with nd4j dependency

--- kmath-nd4j/build.gradle.kts
+++ kmath-nd4j/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 dependencies {
     api(project(":kmath-core"))
     api("org.nd4j:nd4j-api:1.0.0-beta7")
-    testImplementation("org.deeplearning4j:deeplearning4j-core:1.0.0-beta7")
+    testImplementation("org.nd4j:nd4j-native:1.0.0-beta7")
     testImplementation("org.nd4j:nd4j-native-platform:1.0.0-beta7")
     testImplementation("org.slf4j:slf4j-simple:1.7.30")
 }
