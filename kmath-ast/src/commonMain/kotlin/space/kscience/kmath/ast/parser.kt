84fa878ee328f1c6f2ad828a77b83b325df222eb
Iaroslav Postovalov <postovalovya@gmail.com>
2021-04-20 19:14:59 +0700

    Update dependencies

--- kmath-ast/src/commonMain/kotlin/space/kscience/kmath/ast/parser.kt
+++ kmath-ast/src/commonMain/kotlin/space/kscience/kmath/ast/parser.kt
@@ -3,8 +3,6 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-// TODO move to common when https://github.com/h0tk3y/better-parse/pull/37 is merged
-
 package space.kscience.kmath.ast
 
 import com.github.h0tk3y.betterParse.combinators.*
