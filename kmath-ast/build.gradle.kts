e38b2e1c536b2d1f19cb6ae803b257d01368c196
Iaroslav Postovalov <postovalovya@gmail.com>
2021-11-17 19:03:20 +0700

    Only dump ASM classes during tests if according property is passed to Gradle

--- kmath-ast/build.gradle.kts
+++ kmath-ast/build.gradle.kts
@@ -55,9 +55,10 @@ tasks.dokkaHtml {
     dependsOn(tasks.build)
 }
 
-tasks.jvmTest {
-    jvmArgs = (jvmArgs ?: emptyList()) + listOf("-Dspace.kscience.kmath.ast.dump.generated.classes=1")
-}
+if (System.getProperty("space.kscience.kmath.ast.dump.generated.classes") == "1")
+    tasks.jvmTest {
+        jvmArgs = (jvmArgs ?: emptyList()) + listOf("-Dspace.kscience.kmath.ast.dump.generated.classes=1")
+    }
 
 readme {
     maturity = ru.mipt.npm.gradle.Maturity.EXPERIMENTAL
