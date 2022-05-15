d9b719fe5a7a731abd9bc5717ce26f096c73caab
apomytkina <annapoma90@mail.ru>
2022-05-15 16:55:55 +0300

    DoubleBuffer generator was moved to DoubleBuffer file and factory functions were added for LongArray

--- kmath-nd4j/src/main/kotlin/space/kscience/kmath/nd4j/arrays.kt
+++ kmath-nd4j/src/main/kotlin/space/kscience/kmath/nd4j/arrays.kt
@@ -8,3 +8,13 @@ package space.kscience.kmath.nd4j
 import space.kscience.kmath.misc.toIntExact
 
 internal fun LongArray.toIntArray(): IntArray = IntArray(size) { this[it].toIntExact() }
+
+internal fun LongArray.linspace(start: Long, stop: Long) = Array(this.size) {
+    start + it * ((stop - start) / (this.size - 1))
+}
+
+internal fun LongArray.zeros() = Array(this.size) { 0 }
+
+internal fun LongArray.ones() = Array(this.size) { 1 }
+
+internal fun repeat(number: Long, size: Int) = LongArray(size) { number }
