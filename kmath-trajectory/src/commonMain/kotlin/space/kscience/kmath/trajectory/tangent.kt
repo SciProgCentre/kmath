04127fc3f2b781a2e66b95b8b2d63629dca15220
Alexander Nozik <altavir@gmail.com>
2023-02-18 18:53:03 +0300

    Fix tests

--- kmath-trajectory/src/commonMain/kotlin/space/kscience/kmath/trajectory/tangent.kt
+++ kmath-trajectory/src/commonMain/kotlin/space/kscience/kmath/trajectory/tangent.kt
@@ -5,10 +5,7 @@
 
 package space.kscience.kmath.trajectory
 
-import space.kscience.kmath.geometry.Circle2D
-import space.kscience.kmath.geometry.DoubleVector2D
-import space.kscience.kmath.geometry.Euclidean2DSpace
-import space.kscience.kmath.geometry.LineSegment
+import space.kscience.kmath.geometry.*
 import kotlin.math.*
 
 /**
@@ -18,10 +15,14 @@ import kotlin.math.*
 public fun Circle2D.tangentsToCircle(
     other: Circle2D,
 ): Map<DubinsPath.Type, LineSegment<DoubleVector2D>> = with(Euclidean2DSpace) {
+    //return empty map for concentric circles
+    if(center.equalsVector(other.center)) return@tangentsToCircle emptyMap()
+
+    // A line connecting centers
     val line = LineSegment(center, other.center)
-    val d = line.begin.distanceTo(line.end)
+    // Distance between centers
+    val distance = line.begin.distanceTo(line.end)
     val angle1 = atan2(other.center.x - center.x, other.center.y - center.y)
-    var r: Double
     var angle2: Double
     val routes = mapOf(
         DubinsPath.Type.RSR to Pair(radius, other.radius),
@@ -33,30 +34,27 @@ public fun Circle2D.tangentsToCircle(
         for ((route, r1r2) in routes) {
             val r1 = r1r2.first
             val r2 = r1r2.second
-            r = if (r1.sign == r2.sign) {
+            val r = if (r1.sign == r2.sign) {
                 r1.absoluteValue - r2.absoluteValue
             } else {
                 r1.absoluteValue + r2.absoluteValue
             }
-            if (d * d > r * r) {
-                val l = (d * d - r * r).pow(0.5)
-                angle2 = if (r1.absoluteValue > r2.absoluteValue) {
-                    angle1 + r1.sign * atan2(r.absoluteValue, l)
-                } else {
-                    angle1 - r2.sign * atan2(r.absoluteValue, l)
-                }
-                val w = vector(-cos(angle2), sin(angle2))
-                put(
-                    route,
-                    LineSegment(
-                        center + w * r1,
-                        other.center + w * r2
-                    )
+            if (distance <= r) TODO("Intersecting circles are not supported yet")
+            val l = sqrt(distance * distance - r * r)
+            angle2 = if (r1.absoluteValue > r2.absoluteValue) {
+                angle1 + r1.sign * atan2(r.absoluteValue, l)
+            } else {
+                angle1 - r2.sign * atan2(r.absoluteValue, l)
+            }
+            val w = vector(-cos(angle2), sin(angle2))
+            put(
+                route,
+                LineSegment(
+                    center + w * r1,
+                    other.center + w * r2
                 )
-            }
-            else {
-                throw Exception("Circles should not be")
-            }
+            )
+
         }
     }
 }
