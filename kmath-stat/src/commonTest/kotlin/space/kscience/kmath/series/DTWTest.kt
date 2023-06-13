1773b49b1f592001ba2521d7264378ab19031ac6
EjenY-Poltavchiny <poltavtsev.eu@phystech.edu>
2023-06-13 03:39:29 +0300

    Changed tests

--- kmath-stat/src/commonTest/kotlin/space/kscience/kmath/series/DTWTest.kt
+++ kmath-stat/src/commonTest/kotlin/space/kscience/kmath/series/DTWTest.kt
@@ -6,33 +6,31 @@
 package space.kscience.kmath.series
 
 import space.kscience.kmath.nd.*
-import space.kscience.kmath.operations.DoubleField
 import space.kscience.kmath.operations.algebra
 import space.kscience.kmath.operations.bufferAlgebra
-import space.kscience.kmath.structures.DoubleBuffer
 import space.kscience.kmath.structures.asBuffer
 import kotlin.test.Test
-import kotlin.test.assertEquals
+
 
 class DTWTest {
 
     @Test
     fun someData() : Unit {
-        with(Double.algebra.bufferAlgebra) {
+        with(Double.algebra.bufferAlgebra.seriesAlgebra()) {
             val firstSequence: DoubleArray = doubleArrayOf(0.0, 2.0, 3.0, 1.0, 3.0, 0.1, 0.0, 1.0)
             val secondSequence: DoubleArray = doubleArrayOf(1.0, 0.0, 3.0, 0.0, 0.0, 3.0, 2.0, 0.0, 2.0)
 
-            val seriesOne: DoubleBuffer = firstSequence.asBuffer()
-            val seriesTwo: DoubleBuffer = secondSequence.asBuffer()
+            val seriesOne: Series<Double> = firstSequence.asBuffer().moveTo(0)
+            val seriesTwo: Series<Double> = secondSequence.asBuffer().moveTo(0)
 
-            val result = dynamicTimeWarping(seriesOne, seriesTwo)
+            val result = DoubleFieldOpsND.dynamicTimeWarping(seriesOne, seriesTwo)
             println("Total penalty coefficient: ${result.totalCost}")
             print("Alignment: ")
             println(result.alignMatrix)
             for ((i , j) in result.alignMatrix.indices) {
-                    if (result.alignMatrix[i, j] == 1) {
-                        print("[$i, $j] ")
-                    }
+                if (result.alignMatrix[i, j] > 0.0) {
+                    print("[$i, $j] ")
+                }
             }
         }
     }
