ec88d6be9eeaa7d202fb006fad5fa28e6e0ecb70
Alexander Nozik <altavir@gmail.com>
2024-03-27 08:19:22 +0300

    Remove unnecessary reification

--- kmath-coroutines/src/jvmMain/kotlin/space/kscience/kmath/structures/LazyStructureND.kt
+++ kmath-coroutines/src/jvmMain/kotlin/space/kscience/kmath/structures/LazyStructureND.kt
@@ -48,13 +48,13 @@ public suspend fun <T> StructureND<T>.await(index: IntArray): T =
  * PENDING would benefit from KEEP-176
  */
 @OptIn(PerformancePitfall::class)
-public inline fun <T, reified R> StructureND<T>.mapAsyncIndexed(
+public inline fun <T, R> StructureND<T>.mapAsyncIndexed(
     scope: CoroutineScope,
     crossinline function: suspend (T, index: IntArray) -> R,
 ): LazyStructureND<R> = LazyStructureND(scope, shape) { index -> function(get(index), index) }
 
 @OptIn(PerformancePitfall::class)
-public inline fun <T, reified R> StructureND<T>.mapAsync(
+public inline fun <T, R> StructureND<T>.mapAsync(
     scope: CoroutineScope,
     crossinline function: suspend (T) -> R,
 ): LazyStructureND<R> = LazyStructureND(scope,  shape) { index -> function(get(index)) }
