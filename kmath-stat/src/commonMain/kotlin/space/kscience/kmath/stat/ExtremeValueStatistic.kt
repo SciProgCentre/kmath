57e48194303432cc279088217f7843d291d9c218
qwazer <resheto@gmail.com>
2025-04-15 19:50:19 +0300

    Add "min" and "max" statistics implemented as ExtremeValueStatistic
    - Add ExtremeValueStatistic
    - Add statistics docs

---
+++ kmath-stat/src/commonMain/kotlin/space/kscience/kmath/stat/ExtremeValueStatistic.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018-2025 KMath contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package space.kscience.kmath.stat
+
+import space.kscience.kmath.operations.*
+import space.kscience.kmath.structures.*
+
+/**
+ * Extreme element (minimum or maximum) of non-empty input data according to the provided Comparator.
+ */
+public class ExtremeValueStatistic<T>(
+    private val comparator: Comparator<T>,
+): ComposableStatistic<T, T, T>, BlockingStatistic<T, T> {
+
+    override fun evaluateBlocking(data: Buffer<T>): T {
+        require(data.size > 0) { "Data must not be empty" }
+        var res = data[0]
+        for (i in data.indices) {
+          if (comparator.compare(data[i], res) < 0) {
+              res = data[i]
+          }
+        }
+        return res
+    }
+
+    override suspend fun computeIntermediate(data: Buffer<T>): T = evaluateBlocking(data)
+
+    override suspend fun composeIntermediate(first: T, second: T): T {
+        if (comparator.compare(first, second) < 0) {
+            return first
+        } else{
+            return second
+        }
+    }
+
+    override suspend fun toResult(intermediate: T): T = intermediate
+
+    override suspend fun evaluate(data: Buffer<T>): T = super<ComposableStatistic>.evaluate(data)
+
+}
+
+
+public fun <T> minStatistic(): ExtremeValueStatistic<T> where T : Comparable<T> =
+    ExtremeValueStatistic(naturalOrder())
+
+public fun <T> maxStatistic(): ExtremeValueStatistic<T> where T : Comparable<T> =
+    ExtremeValueStatistic(reverseOrder())
+
+// min
+public val Float64Field.min: ExtremeValueStatistic<Float64> get() = minStatistic()
+public val Int32Ring.min: ExtremeValueStatistic<Int32> get() = minStatistic()
+public val Int64Ring.min: ExtremeValueStatistic<Int64> get() = minStatistic()
+
+// max
+public val Float64Field.max: ExtremeValueStatistic<Float64> get() = maxStatistic()
+public val Int32Ring.max: ExtremeValueStatistic<Int32> get() = maxStatistic()
+public val Int64Ring.max: ExtremeValueStatistic<Int64> get() = maxStatistic()
