59fbe5165f3c76ecf4b9d6e929f46c2e743ac2bb
qwazer <resheto@gmail.com>
2025-04-18 17:10:06 +0300

    add Geometric Mean statistic

---
+++ kmath-stat/src/commonMain/kotlin/space/kscience/kmath/stat/GeometricMean.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018-2025 KMath contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package space.kscience.kmath.stat
+
+import space.kscience.kmath.operations.*
+import space.kscience.kmath.structures.Buffer
+import space.kscience.kmath.structures.Float32
+import space.kscience.kmath.structures.Float64
+import space.kscience.kmath.structures.indices
+
+/**
+ * Geometric mean. Nth root of product of values.
+ */
+public class GeometricMean<T : Comparable<T>>(private val field: ExtendedField<T>) : BlockingStatistic<T, T>, ComposableStatistic<T, T, T> {
+    override fun evaluateBlocking(data: Buffer<T>): T = with(field) {
+        require(data.size > 0) { "Data must not be empty" }
+        var res = zero
+        for (i in data.indices) {
+            if (data[i] < zero) throw ArithmeticException("Geometric mean is not defined for negative numbers. Found: " + data[i])
+            res += ln(data[i])
+        }
+        exp(res / data.size)
+    }
+    override suspend fun computeIntermediate(data: Buffer<T>): T = evaluateBlocking(data)
+
+    override suspend fun composeIntermediate(first: T, second: T): T = with(field){ exp((ln(first) + ln(second))/2) }
+
+    override suspend fun toResult(intermediate: T): T = intermediate
+
+    override suspend fun evaluate(data: Buffer<T>): T = super<ComposableStatistic>.evaluate(data)
+}
+
+public val Float32Field.geometricMean: GeometricMean<Float32> get() = GeometricMean(Float32Field)
+public val Float64Field.geometricMean: GeometricMean<Float64> get() = GeometricMean(Float64Field)
