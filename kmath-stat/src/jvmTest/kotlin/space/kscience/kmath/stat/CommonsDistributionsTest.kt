202585d9564b78df30d66da52d4dc3a279ebd575
qwazer <resheto@gmail.com>
2025-05-10 20:15:33 +0300

    STUD-13 add Quantile
    adapted from Julia implementation

--- kmath-stat/src/jvmTest/kotlin/space/kscience/kmath/stat/CommonsDistributionsTest.kt
+++ kmath-stat/src/jvmTest/kotlin/space/kscience/kmath/stat/CommonsDistributionsTest.kt
@@ -14,19 +14,31 @@ import space.kscience.kmath.samplers.GaussianSampler
 internal class CommonsDistributionsTest {
     @Test
     fun testNormalDistributionSuspend() = runBlocking {
-        val distribution = GaussianSampler(7.0, 2.0)
+        val mu = 7.0; val sigma = 2.0
+        val distribution = GaussianSampler(mu, sigma)
         val generator = RandomGenerator.default(1)
         val sample = distribution.sample(generator).nextBuffer(1000)
-        Assertions.assertEquals(7.0, Mean.evaluate(sample), 0.2)
-        Assertions.assertEquals(2.0, StandardDeviation.evaluate(sample), 0.2)
+        Assertions.assertEquals(mu, Mean.evaluate(sample), 0.2)
+        Assertions.assertEquals(sigma, StandardDeviation.evaluate(sample), 0.2)
+        //the first quartile (Q1)
+        // For a normal distribution with mean \(\mu \) and standard deviation \(\sigma \), the Q1 is approximately \(\mu -0.675\sigma \)
+        Assertions.assertEquals(mu-0.675*sigma, Quantile.evaluate(p=0.25, sample), 1e-1)
+        //the third quartile (Q3)
+        Assertions.assertEquals(mu+0.675*sigma, Quantile.evaluate(p=0.75, sample), 1e-1)
     }
 
     @Test
     fun testNormalDistributionBlocking() {
-        val distribution = GaussianSampler(7.0, 2.0)
+        val mu = 7.0; val sigma = 2.0
+        val distribution = GaussianSampler(mu, sigma)
         val generator = RandomGenerator.default(1)
         val sample = distribution.sample(generator).nextBufferBlocking(1000)
-        Assertions.assertEquals(7.0, Mean.evaluate(sample), 0.2)
-        Assertions.assertEquals(2.0, StandardDeviation.evaluate(sample), 0.2)
+        Assertions.assertEquals(mu, Mean.evaluate(sample), 0.2)
+        Assertions.assertEquals(sigma, StandardDeviation.evaluate(sample), 0.2)
+        //the first quartile (Q1)
+        // For a normal distribution with mean \(\mu \) and standard deviation \(\sigma \), the Q1 is approximately \(\mu -0.675\sigma \)
+        Assertions.assertEquals(mu-0.675*sigma, Quantile.evaluate(p=0.25, sample), 1e-1)
+        //the third quartile (Q3)
+        Assertions.assertEquals(mu+0.675*sigma, Quantile.evaluate(p=0.75, sample), 1e-1)
     }
 }
